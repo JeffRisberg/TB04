ee6dd7db0eec083d13d3c6eaa130d364140537e9
JeffRisberg <jrisberg@aol.com>
2018-01-28 14:59:19 -0800

    updated script-based examples including carousel.

--- page07.php
+++ page07.php
@@ -25,9 +25,9 @@
 
 <body>
 
-<script src="js/jquery-3.3.2.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.js"></script>
 
 <div class="navbar navbar-inverse navbar-fixed-top">
   <div class="container">
